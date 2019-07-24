b7658df2c607846c47dc3f8de0d8e5708c2013ce
Luz <luzma_87@yahoo.com>
2019-07-23 19:52:54 -0500

    moves make tiles map to map generator

--- src/LzMap.js
+++ src/LzMap.js
@@ -1,14 +1,11 @@
-import { padStart } from "lodash";
 import * as PIXI from 'pixi.js';
 import positionHelper from "./helpers/positionHelper";
 import LzMapTile from "./LzMapTile";
-import tiles from "./tiles";
-import chance from "./helpers/chance"
 
 class LzMap {
   constructor(stage, map) {
     this.container = new PIXI.Container();
-    this.tilesMap = this.makeTilesMap(map);
+    this.tilesMap = map;
 
     stage.addChild(this.container);
   }
@@ -27,26 +24,6 @@ class LzMap {
 
   update() {
 
-  }
-
-  makeTilesMap(idsMap) {
-    const tilesMap = [];
-    idsMap.forEach((row, rowIndex) => {
-      let tilesRow = [];
-      row.forEach((tileId, colIndex) => {
-        const tile = {...tiles[tileId]};
-        const {sequence, sprite, length} = tile;
-        let randomIndex = 1;
-        if (!sequence) {
-          randomIndex = chance.integer({min: 1, max: length});
-        }
-        tile.currentSprite = `${sprite}_${padStart(randomIndex.toString(), 2, '0')}`;
-        tile.position = {x: colIndex, y: rowIndex};
-        tilesRow.push(tile);
-      });
-      tilesMap.push(tilesRow);
-    });
-    return tilesMap;
   }
 }
 
--- src/MapGenerator.js
+++ src/MapGenerator.js
@@ -1,9 +1,10 @@
+import { padStart } from "lodash";
+import chance from "./helpers/chance";
+import tiles from "./tiles";
+
 class MapGenerator {
   constructor() {
-  }
-
-  getMap() {
-    const map = [
+    this.map = [
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -20,10 +21,27 @@ class MapGenerator {
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ];
-
-    return map;
   }
 
+  getMap() {
+    const tilesMap = [];
+    this.map.forEach((row, rowIndex) => {
+      let tilesRow = [];
+      row.forEach((tileId, colIndex) => {
+        const tile = {...tiles[tileId]};
+        const {sequence, sprite, length} = tile;
+        let randomIndex = 1;
+        if (!sequence) {
+          randomIndex = chance.integer({min: 1, max: length});
+        }
+        tile.currentSprite = `${sprite}_${padStart(randomIndex.toString(), 2, '0')}`;
+        tile.position = {x: colIndex, y: rowIndex};
+        tilesRow.push(tile);
+      });
+      tilesMap.push(tilesRow);
+    });
+    return tilesMap;
+  }
 }
 
 export default MapGenerator;
